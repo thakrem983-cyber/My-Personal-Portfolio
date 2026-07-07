f570559136400afa1a3fb0c080d1f65ba885bc24
thakrem983-cyber <thakrm983@gmail.com>
2026-07-07 12:31:18 +0530

    Update My Personal Portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+// Download Resume Button
+const resumeBtn = document.getElementById("resumeBtn");
+
+resumeBtn.addEventListener("click", () => {
+    window.open("Khemlata_Thakre_Resume.pdf", "_blank");
+});
+
+//Visit GitHub Button
+const githubBtn = document.getElementById("githubBtn");
+
+githubBtn.addEventListener("click", () => {
+    window.open("https://github.com/thakrem983-cyber", "_blank");
+});
+
+
